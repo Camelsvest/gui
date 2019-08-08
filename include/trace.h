e04279ee1555bd4fe72eab0a2edab588eeddc48a
camelsvest <2945791313@qq.com>
2019-08-08 02:53:40 -0700

    move definition into logging.h

--- include/trace.h
+++ include/trace.h
@@ -3,19 +3,7 @@
 
 #include "logging.h"
 
-#ifdef __WIN32_WINNT
-    #define FILENAME(x) (strrchr(x,'\\')?strrchr(x,'\\')+1:x)
-#else
-    #define FILENAME(x) (strrchr(x,'/')?strrchr(x,'/')+1:x)
-#endif
 
-#define __FILENAME__     (FILENAME(__FILE__))
-
-#define ENTER_CLASS_FUNCTION(class_name) logging_verbos("Enter %s->%s, %s:%u\r\n", class_name, __FUNCTION__, __FILENAME__, __LINE__)
-#define EXIT_CLASS_FUNCTION(class_name)  logging_verbos("Exit %s->%s, %s:%u\r\n", class_name, __FUNCTION__, __FILENAME__, __LINE__);
-
-#define ENTER_FUNCTION  logging_verbos("Enter %s, %s:%u\r\n", __FUNCTION__, __FILENAME__, __LINE__)
-#define EXIT_FUNCTION   logging_verbos("Exit %s, %s:%u\r\n", __FUNCTION__, __FILENAME__, __LINE__);
 
 
 #endif
